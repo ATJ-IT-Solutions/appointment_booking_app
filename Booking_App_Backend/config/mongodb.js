feb37c37f05aa4fdafb7dd3de3677f6b627aa74c
aaron592 <bsilent590@gmail.com>
2025-07-06 22:39:50 +0300

    mogodb connection changes

--- Booking_App_Backend/config/mongodb.js
+++ Booking_App_Backend/config/mongodb.js
@@ -1,18 +1,13 @@
 import mongoose from "mongoose"
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000,
-    })
+const connectDB = async ()=>{
+    mongoose.connection.on('connected', () => console.log("Database Connected"))
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message)
-    process.exit(1) // Or handle gracefully in serverless
-  }
+    await mongoose.connect(`${process.env.MONGODB_URI}`,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000,
+  })
 }
 
 export default connectDB
