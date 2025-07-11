6da0d643adeec97e9cbd3f6a5e6d0a38943cb2d8
aaron592 <bsilent590@gmail.com>
2025-07-11 20:38:39 +0300

    added vercel file

--- Booking_App_Backend/server.js
+++ Booking_App_Backend/server.js
@@ -6,23 +6,19 @@ import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
 import userRouter from './routes/userRoute.js'
 
-//app config
-const app = express()
-const port = process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+const app = express();
+connectDB();
+connectCloudinary();
 
-// app middlewares
-
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
-//api endpoints
-app.use('/api/admin',adminRouter)
-app.use('/api/user',userRouter)
 
-app.get('/',(req,res)=>{
-    res.send("API working")
-})
+app.use('/api/admin', adminRouter);
+app.use('/api/user', userRouter);
+
+app.get('/', (req, res) => {
+  res.send('API working');
+});
 
 app.listen(port, ()=> console.log("Server Started",port))
