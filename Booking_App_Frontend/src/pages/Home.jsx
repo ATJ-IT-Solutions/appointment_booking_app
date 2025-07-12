82fd8d98b65f92fecde5d6afc9471e978c8c5441
aaron592 <bsilent590@gmail.com>
2025-07-12 12:22:28 +0300

    changes for consults count

--- Booking_App_Frontend/src/pages/Home.jsx
+++ Booking_App_Frontend/src/pages/Home.jsx
@@ -48,7 +48,7 @@ const Home = () => {
           <p className="text-sm text-gray-300">Years experience</p>
         </div>
         <div>
-          <h3 className="text-3xl font-bold">600+</h3>
+          <h3 className="text-3xl font-bold">10,000+</h3>
           <p className="text-sm text-gray-300">Consults done</p>
         </div>
       </div>
@@ -96,7 +96,7 @@ const Home = () => {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="w-4 h-4" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
               </svg>
-              {item.consults || "679"} Consults done
+              {item.consults || "10,000 +"} Consults done
             </div>
              {/* Book Slot Button */}
             <button
